b32fa3186a2e48a06168cbcf32a27cf4ffc46007
Cesariux23 <cesariux23@gmail.com>
2016-12-18 15:18:48 -0600

    asset a moment

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -93,8 +93,7 @@
 		<!-- Scripts -->
 		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 		<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
-		<script src="/js/moment-with-locales.js"></script>
-
+		<script src="{{ asset('js/moment-with-locales.js')}}"></script>
 
 <script type="text/javascript">
         //language:  'es',
