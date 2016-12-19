5a15770bd8c54e2c819606c490dfa797f6a96cfb
USUARIO-OA3D7IM\amado <crisfranco3@hotmail.com>
2016-12-19 09:38:50 -0600

    routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,9 +21,6 @@ Route::get('/', 'HomeController@index');
 Route::resource('empresa', 'EmpresaController');
 Route::resource('ventas', 'VentasController');
 Route::resource('producto', 'ProductoController');
-<<<<<<< HEAD
 Route::resource('meseros', 'MeseroController');
-=======
 Route::resource('meseros', 'MeseroController');
 Route::resource('mesa', 'MesaController');
->>>>>>> 460b5e22e3417f7dd1173650d105d4b0ac60e2f8
